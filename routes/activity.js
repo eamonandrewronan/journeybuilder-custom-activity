d58f2dab6ad009cf533e02a3ac07642cb52bf929
AzureAD\LiamCollerton <liam.collerton@giveclarity.org>
2022-05-09 12:52:48 +0100

    Fix

--- routes/activity.js
+++ routes/activity.js
@@ -25,6 +25,43 @@ exports.execute = async (req, res) => {
       method = 'FTP';
     }
 
+//    var id = data.inArguments[0].contactKey;
+
+    var id = '00306000025YjA9AAK';
+
+    var conn = new jsforce.Connection({
+      // you can change loginUrl to connect to sandbox or prerelease env.
+     loginUrl : 'https://login.salesforce.com'
+    });
+  
+    conn.login('liam.collerton@gcdemo.org', 'dt#UnjEc2*CMck1!6#LDCO7eMuJicdnB9tzCCzYlq3Egly', function(err, res) {
+      if (err) { return console.error(err); }
+  
+
+      var records = [];
+      conn.query("SELECT Id FROM Contact where id ='" + id + "'", function(err, result) {
+        if (err) { return console.error(err); }
+        console.log("total : " + result.totalSize);
+        console.log("fetched : " + result.records.length);
+        console.log("done ? : " + result.done);
+
+        var newId = result.records[0].id;
+
+        // Single record update
+        conn.sobject("Contact").update({ 
+          Id : newId,
+          Title : 'Lord'
+        }, function(err, ret) {
+          if (err || !ret.success) { return console.error(err, ret); }
+          console.log('Updated Successfully : ' + ret.id);
+          // ...
+        });
+
+      });
+
+    });
+  
+
     await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
       {
         keys: {
@@ -88,6 +125,10 @@ exports.publish = (req, res) => {
  */
 exports.validate = (req, res) => {
 
+  logger.info('validate');
+
+  var id = '00306000025YjA9AAK';
+
   var conn = new jsforce.Connection({
     // you can change loginUrl to connect to sandbox or prerelease env.
    loginUrl : 'https://login.salesforce.com'
@@ -96,21 +137,29 @@ exports.validate = (req, res) => {
   conn.login('liam.collerton@gcdemo.org', 'dt#UnjEc2*CMck1!6#LDCO7eMuJicdnB9tzCCzYlq3Egly', function(err, res) {
     if (err) { return console.error(err); }
 
-    // Single record update
-    conn.sobject("Contact").update({ 
-      Id : '00306000025YjA9AAK',
-      Title : 'Lord'
-    }, function(err, ret) {
-      if (err || !ret.success) { return console.error(err, ret); }
-      console.log('Updated Successfully : ' + ret.id);
-      // ...
+
+    var records = [];
+    conn.query("SELECT Id FROM Contact where id ='" + id + "'", function(err, result) {
+      if (err) { return console.error(err); }
+      console.log("total : " + result.totalSize);
+      console.log("fetched : " + result.records.length);
+      console.log("done ? : " + result.done);
+
+      var newId = result.records[0].id;
+
+      // Single record update
+      conn.sobject("Contact").update({ 
+        Id : newId,
+        Title : 'Sir'
+      }, function(err, ret) {
+        if (err || !ret.success) { return console.error(err, ret); }
+        console.log('Updated Successfully : ' + ret.id);
+        // ...
+      });
+
     });
+
   });
-
-
-  logger.info(JSON.stringify(req.body));
-
-  logger.info('validate');
 
   res.status(200).send({
     status: 'ok',
