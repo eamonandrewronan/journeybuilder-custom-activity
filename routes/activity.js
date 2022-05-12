1a86f9d412bb2798077f80f82ba51f09d35e3321
AzureAD\LiamCollerton <liam.collerton@giveclarity.org>
2022-05-12 14:52:02 +0100

    Update log DEs

--- routes/activity.js
+++ routes/activity.js
@@ -73,19 +73,29 @@ exports.execute = async (req, res) => {
 
     });
 
-    await SFClient.saveData(process.env.DATA_EXTENSION_EXTERNAL_KEY, [
+    await SFClient.saveData(process.env.LOGGING_DATA_EXTENSION, [
       {
         keys: {
           SubscriberKey: data.inArguments[0].contactIdentifier,
         },
         values: {
-          Vendor: data.inArguments[0].DropdownOptions,
-          Communication: data.inArguments[0].DropdownCommunications,
-          Method: method,
+          Message: ' - Updated from JB.' + 'Vendor: ' + data.inArguments[0].DropdownOptions + ', Communication: ' + data.inArguments[0].DropdownCommunications + ', Method: ' + method,
         },
       },
     ]);
   } catch (error) {
+
+    await SFClient.saveData(process.env.LOGGING_DATA_EXTENSION, [
+      {
+        keys: {
+          SubscriberKey: data.inArguments[0].contactIdentifier,
+        },
+        values: {
+          Message: error,
+        },
+      },
+    ]);
+
     logger.error(error);
   }
 
